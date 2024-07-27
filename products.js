09fe1d33a60f8baa5d7195f7d4cd1753b35f2884
sabeerkhan2003 <sabeersabee20@gmail.com>
2024-07-27 13:45:50 +0530

    product functionality

--- products.js
+++ products.js
@@ -9,3 +9,27 @@ menu.addEventListener("click", function () {
 navx.addEventListener("click", function () {
     options.style.right = "-50%"
 })
+
+// products functionality
+
+var products=document.getElementById("products")
+var search=document.getElementById("search")
+var productname=products.querySelectorAll("div")
+
+    search.addEventListener("keyup",function(){
+        var enteredvalue=event.target.value.toUpperCase()
+
+        for(count=0;count<productname.length;count=count+1)
+        {
+            var prod=productname[count].querySelector("h2").textContent
+             
+            if (prod[count].toUpperCase().indexOf(enteredvalue)<0)
+            {
+                productname[count].style.display="none"
+            }
+            else{
+                productname[count].style.display="block"
+            }
+        }
+
+})
